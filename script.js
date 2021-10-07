ff7cd6c3651d54d502076c5a35e20509e6517100
Deep Patel <77803421+DeepPatel2142@users.noreply.github.com>
2021-10-06 22:47:42 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+var typed = new Typed(".typing", {
+    strings: ["adaptive to new technology!","passionate about the programming :)"],
+    typeSpeed: 80,
+    backSpeed: 40,
+    loop: true
+});
